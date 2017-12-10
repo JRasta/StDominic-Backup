7e1644d2eff52c0e10f18698bebd5c9d5269b334
HRoughDiamond <jaharper88@gmail.com>
2017-12-10 20:42:02 +0000

    HelloWorld introduction Program
    Base program for introducing coding 
    -Print to console
    -Read from console
    -Declare Variables
    -Methods/Classes

---
+++ HelloWorld/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HelloWorld
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //string myFirstString;
+
+            Console.WriteLine("Hello World");
+            
+            //string myFirstString = createMeInCode();
+            //Console.WriteLine("Hello " + myFirstString);
+
+
+
+            Console.ReadLine();
+        }
+
+        static string createMeInCode()
+        {
+            string myName = "";
+
+            Console.WriteLine("Please insert a name");
+            myName = Console.ReadLine();
+
+            return myName;
+        }
+    }
+}
